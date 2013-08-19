33264693627dd26a483854b86b3f223e2dc0b3f0
celine <wenchih.hsieh611@gmail.com>
2013-08-19 23:20:16 +0800

    complete widget

--- src/com/inventions/taiwansatellites/LoadSatellite.java
+++ src/com/inventions/taiwansatellites/LoadSatellite.java
@@ -2,12 +2,20 @@ package com.inventions.taiwansatellites;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLConnection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import com.squareup.picasso.Cache;
 import com.squareup.picasso.LruCache;
@@ -27,24 +35,39 @@ public class LoadSatellite {
 	static final String PREFIX = "http://www.cwb.gov.tw";
 	private static final String LOG_TAG = "LoadSatellite";
 	Context mContext;
+	private final File PARENT_DIRECTORY;
 
 	public LoadSatellite(Context context) {
 		mContext = context;
+		PARENT_DIRECTORY = new File(context.getCacheDir(), "satellites");
 	}
 
-	public void loadSatellites() {
+	public List<String> loadSatellitesPath() {
 		String pics[] = getAllSatellites();
+		List<String> filePath = new ArrayList<String>();
 		Uri uris[] = new Uri[pics.length];
 		int i = 0;
 		try {
 			for (String pic : pics) {
 				Log.d(LOG_TAG, "pic " + pic);
+				String urltime[] = pic.split(":");
 				String uri = PREFIX
-						+ pic.substring(pic.indexOf('"') + 1,
-								pic.indexOf('"', 2));
+						+ urltime[0].substring(1, urltime[0].length() - 1);
 				Log.d(LOG_TAG, "uri " + uri);
+				String time = urltime[1].substring(1, urltime[1].length() - 1);
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+				Date date;
+				try {
+					date = sdf.parse(time);
+					URLConnection conn = new URI(uri).toURL().openConnection();
+					conn.connect();
+					filePath.add(writeToFile(conn.getInputStream(),
+							String.valueOf(date.getTime())));
 
-				URLConnection conn = new URI(uri).toURL().openConnection();
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
 				i++;
 			}
@@ -58,6 +81,7 @@ public class LoadSatellite {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return filePath;
 	}
 
 	static int BUFFSIZE = 125;
@@ -80,22 +104,22 @@ public class LoadSatellite {
 		return null;
 	}
 
-	private static void writeToFile(InputStream in) {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		FileOutputStream fos = new FileOutputStream(out);
+	private String writeToFile(InputStream in, String filename)
+			throws FileNotFoundException {
+		File file = new File(PARENT_DIRECTORY, filename);
+		FileOutputStream fos = new FileOutputStream(file);
 		byte data[] = new byte[BUFFSIZE];
 		int byteRead = 0;
 		try {
 			while ((byteRead = in.read(data)) > 0) {
-				out.write(data);
+				fos.write(data, 0, byteRead);
 			}
-			byte result[] = out.toByteArray();
+			fos.flush();
 
-			return new String(result);
 		} catch (IOException e) {
 			Log.e(LOG_TAG, "error", e);
 		}
-		return null;
+		return file.getPath();
 	}
 
 	public String[] getAllSatellites() {
--- src/com/inventions/taiwansatellites/SatelliteWidget.java
+++ src/com/inventions/taiwansatellites/SatelliteWidget.java
@@ -1,13 +1,16 @@
 package com.inventions.taiwansatellites;
 
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.widget.RemoteViews;
 
 public class SatelliteWidget extends AppWidgetProvider {
@@ -17,29 +20,47 @@ public class SatelliteWidget extends AppWidgetProvider {
 			int[] appWidgetIds) {
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
 		LoadSatellite loader = new LoadSatellite(context);
-		loader.loadSatellites();
+		List<String> files = loader.loadSatellitesPath();
 		Timer timer = new Timer();
-		timer.scheduleAtFixedRate(new UpdateTask(context, appWidgetManager), 1,
-				1000);
+		timer.scheduleAtFixedRate(new UpdateTask(context, appWidgetManager,
+				files), 1, 1000);
 	}
 
 	public static class UpdateTask extends TimerTask {
 		RemoteViews mView;
 		AppWidgetManager mManager;
 		Context mContext;
+		int mIndex = 0;
+		List<String> filePath;
 
-		public UpdateTask(Context context, AppWidgetManager manager) {
+		public UpdateTask(Context context, AppWidgetManager manager,
+				List<String> files) {
 			mManager = manager;
 			mContext = context;
 			mView = new RemoteViews(context.getPackageName(),
 					R.layout.satellite_img);
+			filePath = files;
+			mIndex = 0;
 		}
 
 		@Override
 		public void run() {
-			File file = new File();
-			Picasso.with(mContext).load(file).into();
-			mView.setImageViewBitmap(R.id.img, bitmap);
+			Target target = new Target() {
+
+				@Override
+				public void onSuccess(Bitmap bitmap) {
+					mView.setImageViewBitmap(R.id.img, bitmap);
+
+				}
+
+				@Override
+				public void onError() {
+					// TODO Auto-generated method stub
+
+				}
+
+			};
+			Picasso.with(mContext).load(filePath.get(mIndex)).into(target);
 
 		}
 	}
